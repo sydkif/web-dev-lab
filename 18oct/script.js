c9ba89b6df9635fd367cb4003844610345ac204c
Syed Akif <57567504+sydkif@users.noreply.github.com>
2021-10-21 01:38:24 +0800

    Added sound

--- 18oct/script.js
+++ 18oct/script.js
@@ -1,27 +1,35 @@
 // https://www.w3schools.com/howto/howto_css_modals.asp
 
 // Get the modal
-var modal = document.getElementById("myTv");
+var tv = document.getElementById("myTv");
+var radio = document.getElementById("myRadio");
 
 // Get the button that opens the modal
-var btn = document.getElementById("watchTV");
+var tvBtn = document.getElementById("watchTV");
+var rdBtn = document.getElementById("playRadio");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+tvBtn.onclick = function () {
+    tv.style.display = "block";
+}
+rdBtn.onclick = function () {
+    radio.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    tv.style.display = "none";
+    radio.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == tv)
+        tv.style.display = "none";
+    if (event.target == radio)
+        radio.style.display = "none";
+
 }
